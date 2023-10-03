a1eee84acf9ecd3d3f03eb5c0fa7e7b848e6c928
alkhalifas <salehesam@gmail.com>
2023-10-03 14:18:47 -0400

    added chart

--- src/containers/Page1.jsx
+++ src/containers/Page1.jsx
@@ -1,13 +1,48 @@
-// src/containers/Home.jsx
 import React from 'react';
-import {Link} from "react-router-dom";
-import {Button} from "@mui/material";
+import { Link } from 'react-router-dom';
+import { Button } from '@mui/material';
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+
+const data = [
+    { name: 'Jan', uv: 4000, pv: 2400, amt: 2400 },
+    { name: 'Feb', uv: 3000, pv: 1398, amt: 2210 },
+    { name: 'Mar', uv: 2000, pv: 9800, amt: 2290 },
+    { name: 'Apr', uv: 2780, pv: 3908, amt: 2000 },
+    { name: 'May', uv: 1890, pv: 4800, amt: 2181 },
+    { name: 'Jun', uv: 2390, pv: 3800, amt: 2500 },
+    { name: 'Jul', uv: 3490, pv: 4300, amt: 2100 },
+];
 
 const Page1 = () => {
     return (
-        <div style={{justifyContent: 'center'}}>
+        <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
             <h2>Page 1</h2>
 
+            <ResponsiveContainer width="90%" height={400}>
+                <LineChart
+                    width={500}
+                    height={300}
+                    data={data}
+                    margin={{
+                        top: 5,
+                        right: 30,
+                        left: 20,
+                        bottom: 5,
+                    }}
+                >
+                    <CartesianGrid strokeDasharray="3 3" />
+                    <XAxis dataKey="name" />
+                    <YAxis />
+                    <Tooltip />
+                    <Legend />
+                    <Line type="monotone" dataKey="uv" stroke="#8884d8" activeDot={{ r: 8 }} />
+                    <Line type="monotone" dataKey="pv" stroke="#82ca9d" />
+                </LineChart>
+            </ResponsiveContainer>
+
+            <Button component={Link} to="/" color="primary" variant="contained" style={{ marginTop: '20px' }}>
+                Go Back
+            </Button>
         </div>
     );
 };
